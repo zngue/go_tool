3ce1b62edc90ea21ccb2118a059fc0e744021b2c
zngue <zngfue@163.com>
2020-07-10 10:31:17 +0800

    新增统一修改状态 或者删除实体

---
+++ src/common/request/status.go
@@ -0,0 +1,29 @@
+package request
+
+import "github.com/zngue/go_tool/src/fun/zng_str"
+//用户删除或者修改状态通用
+type IDStatusRequest struct {
+	ID int `form:"id" binding:"required"`
+	Status int `form:"status"`//1 正常  2 禁用
+	From int `form:"form"` // 1 修改状态 2 是删除
+	Type int `form:"type"`//来源  0 单个id 1 使用数组 id字符串
+	IDArr []int
+	IDString string `form:"id_string"`
+	Def string `form:"def"` //字符串分隔符 默认 英文逗号分隔
+}
+func (s *IDStatusRequest ) IDStringToIDArr () *IDStatusRequest  {
+	if s.Def!="" {
+		s.IDArr = zng_str.IDStringToSlice(s.IDString,s.Def)
+	}else{
+		s.IDArr = zng_str.IDStringToSlice(s.IDString,",")
+	}
+	return s
+}
+func (s *IDStatusRequest)  CheckStatus() *IDStatusRequest  {
+	if s.Type==1 {
+		s.IDStringToIDArr()
+	}
+	return s
+}
+
+
