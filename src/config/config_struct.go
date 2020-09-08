ed29f049f37654492a0f7bba46d5e46f40e914eb
zngue <zngfue@163.com>
2020-09-08 14:49:34 +0800

    Update config_struct.go

--- src/config/config_struct.go
+++ src/config/config_struct.go
@@ -4,7 +4,7 @@ import "crypto/rsa"
 
 type Config struct {
 	Mysql Mysql `json:"mysql" `
-	Redis Redis	`json:"redis.yaml" `
+	Redis Redis	`json:"redis" `
 	System System `json:"system" `
 	HttpRequest HttpRequest  `json:"http_request" `
 	ServiceList []ServiceList `json:"serviceList" `
@@ -12,21 +12,21 @@ type Config struct {
 	AliyunOss AliyunOss `json:"aliyunoss" `
 	JWT Jwt `json:"jwt"`
 	AliAppClient AliAppClient `json:"ali_app_client"`
-	WeChat WeChat 	`json:"wechat"`
+	WeChat WeChat 	`json:"weChat"`
 }
 //数据库配置信息
 type Mysql struct {
-	DBName string `json:"dbName"`
-	Username string `json:"username"`
-	Password string `json:"password"`
-	Config string	`json:"config"`
-	Host   string `json:"host"`
-	Port	string `json:"port"`
-	LogMode bool `json:"logMode"`
-	MaxIdleConns int `json:"maxIdleConns"`
-	MaxOpenConns int `json:"maxOpenConns"`
-	Charset string `json:"charset"`
-	TimeStamp bool `json:"timeStamp"`
+	DBName string `json:"dbName" form:"db_name" gorm:"column:db_name"`
+	Username string `json:"username" form:"user_name" gorm:"column:user_name"`
+	Password string `json:"password" form:"password" gorm:"column:password"`
+	Config string	`json:"config" form:"config" gorm:"column:config"`
+	Host   string `json:"host" form:"host" gorm:"column:host"`
+	Port	string `json:"port" form:"port" gorm:"column:port"`
+	LogMode bool `json:"logMode" form:"log_mode" gorm:"column:log_mode"`
+	MaxIdleConns int `json:"maxIdleConns" form:"max_idle_conns" gorm:"column:max_idle_conns"`
+	MaxOpenConns int `json:"maxOpenConns" form:"max_open_conns" gorm:"column:max_open_conns"`
+	Charset string `json:"charset" form:"charset" gorm:"column:charset"`
+	TimeStamp bool `json:"timeStamp" form:"time_stamp" gorm:"column:time_stamp"`
 }
 type AliAppClient struct {
 	PartnerID  string  `json:"partner_id"`//合作者ID
@@ -37,55 +37,54 @@ type AliAppClient struct {
 }
 //redis配置信息
 type Redis struct {
-	Host   string `json:"host"`
-	Port	string `json:"port"`
-	Password string `json:"password"`
-	DBNum	int `json:"dbNum"`
-	PoolSize int `json:"poolSize"`
+	Host   string `json:"host" gorm:"column:host"  form:"host"`
+	Port	string `json:"port"  gorm:"column:port"  form:"port"`
+	Password string `json:"password" gorm:"column:password"  form:"password"`
+	DBNum	int `json:"dbNum" gorm:"column:db_num"  form:"db_num"`
+	PoolSize int `json:"poolSize" gorm:"column:pool_size"  form:"pool_size"`
 }
 //系统配置信息
 type System struct {
-	Port string `json:"port"`
+	Port string `json:"port" form:"desc" gorm:"column:desc"`
 }
 //http请求配置信息
 type HttpRequest struct {
-	ServiceMode	bool `json:"serviceMode"`
-	RedisDBNum int `json:"redisDbNum"`
+	ServiceMode	bool `json:"serviceMode" gorm:"column:redis_db_num"  form:"redis_db_num"`
+	RedisDBNum int `json:"redisDbNum" gorm:"column:service_mode" form:"service_mode"`
 }
 //http 服务请求配置
 type ServiceList struct {
-	Name string `json:"name"`
-	Url string `json:"url"`
+	Name string `json:"name" gorm:"column:name" form:"name"`
+	Url string `json:"url" gorm:"column:url" form:"url"`
 }
 //默认加载数据
 type DefaultLoad struct {
-	Mysql bool `json:"mysql"`
-	Redis bool `json:"redis"`
+	Mysql bool `json:"mysql" gorm:"column:mysql"  form:"mysql"`
+	Redis bool `json:"redis" gorm:"column:redis" form:"redis"`
 }
 type AliyunOss struct {
-	Accessid string `json:"accessid"`
-	Accesskey string `json:"accesskey"`
-	Endpoint string `json:"endpoint"`
-	Bucket string `json:"bucket"`
-	Uploaddir string `json:"uploaddir"`
-	DomainName string `json:"domainName"`
-	Ssl bool `json:"ssl"`
+	Accessid string `json:"accessid" gorm:"auto_increment"   form:"accessid"`
+	Accesskey string `json:"accesskey" gorm:"column:accesskey" form:"accesskey"`
+	Endpoint string `json:"endpoint" gorm:"column:endpoint" form:"endpoint"`
+	Bucket string `json:"bucket" gorm:"column:bucket" form:"bucket"`
+	Uploaddir string `json:"uploaddir" gorm:"column:upload_dir" form:"upload_dir"`
+	DomainName string `json:"domainName" gorm:"column:domain_name" form:"domain_name"`
+	Ssl bool `json:"ssl" gorm:"column:ssl" form:"ssl"`
 }
 type Jwt struct {
-	Secret string `json:"secret"`
-	ExpireTime int `json:"expireTime"`
-	Issuer string `json:"issuer"`
-	Subject string `json:"subject"`
+	Secret string  `json:"secret" gorm:"column:secret" form:"secret"`
+	ExpireTime int `json:"expireTime" gorm:"column:expire_time" form:"expire_time"`
+	Issuer string  `json:"issuer" gorm:"column:issuer" form:"issuer"`
+	Subject string `json:"subject" gorm:"column:subject" form:"subject"`
 }
 type WeChat struct {
-	AppID string `json:"appID"`
-	AppSecret string `json:"appSecret"`
-	Token string `json:"token"`
-	EncodingAESKey string `json:"encodingAESKey"`
-	PayMchID string `json:"payMchID"`
-	PayNotifyURL string `json:"payNotifyURL"`
-	PayKey string `json:"payKey"`
-	RedisNum int `json:"redisNum"`
-
+	AppID string `json:"appID" gorm:"column:app_id" form:"app_id"`
+	AppSecret string `json:"appSecret" gorm:"column:app_secret" form:"app_secret"`
+	Token string `json:"token" gorm:"column:token" form:"token"`
+	EncodingAESKey string `json:"encodingAESKey" gorm:"column:encoding_aes_key" form:"encoding_aes_key"`
+	PayMchID string `json:"payMchID"  gorm:"column:pay_mch_id" form:"pay_mch_id"`
+	PayNotifyURL string `json:"payNotifyURL"  gorm:"column:pay_notify_url" form:"pay_notify_url"`
+	PayKey string `json:"payKey"  gorm:"column:pay_key" form:"pay_key"`
+	RedisNum int `json:"redisNum" gorm:"column:redis_num" form:"redis_num"`
 }
 
